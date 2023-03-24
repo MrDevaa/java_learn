0895780cc285ee9f3322219f77670d8807bb8985
MrDevaa <d.codandabany@gmail.com>
2023-03-24 18:47:48 +0100

    DataTypes

---
+++ datatype.java
@@ -0,0 +1,27 @@
+import org.w3c.dom.views.DocumentView;
+
+class datatype 
+{
+    public static void main(String a []) 
+    {
+    //integer 
+     int num1 = 9;
+     //byte
+     byte by = 127;
+     // short
+     short sh = 558;
+     //long
+     long l = 5854l;
+
+     //float and double
+     float f = 5.8f;
+     double d = 5.8;
+     
+     //character
+     char c = 'k';
+
+     //boolean
+     boolean b = true;
+    }
+    
+}
