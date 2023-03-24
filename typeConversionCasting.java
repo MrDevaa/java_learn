352c0af885b7d6e11f8444dcb9cde20b1f1d462c
MrDevaa <d.codandabany@gmail.com>
2023-03-24 21:12:45 +0100

    Type Conversion and Casting

---
+++ typeConversionCasting.java
@@ -0,0 +1,23 @@
+class TypeConversionCasting 
+{
+    public static void main(String args []) 
+    {
+        // byte b = 125;
+        // int  a = 257;
+
+        // byte k = (byte)a;
+
+        // float f = 5.6f;
+        //     int t = (int)f;
+
+        // conversion
+        byte a = 10;
+        byte b = 30;
+
+         int result = a*b;
+        System.out.println(result);
+    }
+    
+}
+
+// first compiled the code and run the code.
