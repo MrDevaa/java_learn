18c0598f738b4b35f3fb0c41d1d50c17fbbd44ed
MrDevaa <d.codandabany@gmail.com>
2023-03-25 01:23:21 +0100

    relational operator

---
+++ relationOperator.java
@@ -0,0 +1,17 @@
+public class relationOperator 
+{
+    public static void main(String [] args) 
+    {
+        double x = 8.8;
+        double y = 9.8;
+
+        int a = 8;
+        int b = 6;
+        
+
+        boolean result = x >= y;
+
+        System.out.println(result);
+
+    }
+}
