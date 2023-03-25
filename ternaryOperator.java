71a8bbfa1576aaf13d2ac0a837e90a52946af334
MrDevaa <d.codandabany@gmail.com>
2023-03-25 23:55:38 +0100

    tenary Operators

--- ternaryOperator.java
+++ ternaryOperator.java
@@ -2,6 +2,19 @@ public class ternaryOperator
 {
     public static void main(String [] args) 
     {
-        
+       int n = 5;
+       int result = 0;
+
+    // ternaire condition ?;
+
+       result = n%2==0?  10 : 20 ;
+
+
+    //    if(n%2==0)
+    //    result = 10;
+    //    else 
+    //    result = 20;
+
+       System.out.println(result);
     }
 }
