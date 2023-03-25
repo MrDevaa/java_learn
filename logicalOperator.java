f9156973e1d5ed8376ea37f7876a537723c30abf
MrDevaa <d.codandabany@gmail.com>
2023-03-25 01:41:46 +0100

    logical operator

---
+++ logicalOperator.java
@@ -0,0 +1,20 @@
+public class logicalOperator 
+{
+    public static void main(String [] args) 
+    {
+        int x = 7;
+        int y = 5;
+
+        int a = 5;
+        int b = 9;
+        
+
+        // boolean result = x > y && a < b;
+        //  boolean result = x < y || a < b || a > 1;
+         boolean result = a > b;
+
+
+        System.out.println(!result);
+
+    }
+}
