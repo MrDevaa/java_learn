4688fddb85d78bc5a2233037a8fd6c75e110f8e6
MrDevaa <d.codandabany@gmail.com>
2023-03-25 01:07:48 +0100

    assignmrny operationals

---
+++ assignmentOperators.java
@@ -0,0 +1,25 @@
+class assignmentOperators {
+    public static void main(String args[]) 
+    {
+        int num = 7;
+        // int num2 = 5;
+
+        // int result = num1 % num2 ;
+
+        // num1 = num1 + 2;
+
+        // num1 += 1;
+
+        // num1++; // post  - increment
+        // ++num;  // Pre - increment
+
+        // num1--; // decrement
+
+        int result = num++;   // fetch the value and the increment
+        // int result = ++num; // increment and then fetch the value
+
+        System.out.println(result);
+
+    }
+    
+}
