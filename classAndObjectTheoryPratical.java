b5055e3da7951d6f67a5b50590869c5812f32417
MrDevaa <d.codandabany@gmail.com>
2023-03-28 00:21:41 +0200

    how to create claass and object

---
+++ classAndObjectTheoryPratical.java
@@ -0,0 +1,48 @@
+
+
+class Calculator
+{
+
+    int a;
+
+    public int add (int n1, int n2)
+    {
+        int r = n1 + n2;
+        
+    return r;
+
+    }
+
+}
+
+public class classAndObjectTheoryPratical{
+
+public static void main(String a[]) 
+{
+    int num1 =4;
+    int num2 =5;
+
+    Calculator calc = new Calculator();
+
+    int result = calc.add(num1, num2);
+
+   // int result = num1 + num2;
+
+    System.out.println(result);
+
+}
+
+}
+
+
+
+
+
+
+// Object Oriented Programming
+// Object - Properties and Behaviours
+
+
+// Class
+
+// Who create Objects on Java is JVM
