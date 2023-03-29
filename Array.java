d45e258a2440500e15c9c348e7d6de72d8ab7690
MrDevaa <d.codandabany@gmail.com>
2023-03-29 02:21:25 +0200

    Arrays and Multi-Dimensionell Arrays

---
+++ Array.java
@@ -0,0 +1,19 @@
+
+public class Array 
+{
+    public static void main(String a[]) 
+    {
+        int nums [] = new int [4];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+
+        for (int i = 0; i<=3; i++)
+        {
+            System.out.println(nums[i]);
+        }
+        
+    }
+    
+}
