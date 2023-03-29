82a33bf4deef7de66c0b1998332113bee2e53909
MrDevaa <d.codandabany@gmail.com>
2023-03-30 01:11:18 +0200

    String in Java

---
+++ string.java
@@ -0,0 +1,30 @@
+
+
+public class string 
+{
+    public static void main(String a[]) 
+    {
+        // object string
+    //   String  name = new String("Devaa");
+     
+    
+        String name = "devaaaaaa";
+
+      System.out.println("hello " + name);
+
+
+
+
+      // concatinate
+     // System.out.println(name.concat(" reddy"));
+
+
+
+      //index of
+      //System.out.println( name.charAt(1));
+
+
+      //hash code
+       // System.out.println(name.hashCode());
+    }
+}
