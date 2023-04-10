bef40106b414222d35c3e771521515bb5cf55032
MrDevaa <d.codandabany@gmail.com>
2023-04-10 15:30:50 +0200

    this and super in java

---
+++ demo2.java
@@ -0,0 +1,42 @@
+
+class A extends Object
+{
+    public A() 
+    {
+        super();
+        System.out.println("in A");
+    }
+    public A(int n)
+    {
+        super();
+        System.out.println("in A int");
+
+    }
+}
+
+class B extends A 
+{
+    public B()
+    {
+        super();
+        System.out.println("in B");
+    }
+    public B(int n)
+    {
+        this();
+        System.out.println("in B int");
+        
+    }
+}
+
+public class demo2 
+{
+    public static void main(String a[])
+    {
+        B obj = new B();
+    }
+    
+}
+
+
+// every constuctor in java as a method called super.
