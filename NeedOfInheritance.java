05931d992f6d7e4a6e2fc88a1a95c3a20b4198bd
MrDevaa <d.codandabany@gmail.com>
2023-04-12 20:47:48 +0200

    inheritance in java

---
+++ NeedOfInheritance.java
@@ -0,0 +1,36 @@
+public class NeedOfInheritance {
+    
+}
+
+
+/*
+// parents class,  Super class , Base class
+
+class Clac
+{
+    add();
+    sub();
+    multi();
+    div();
+}
+
+// child class, sub class, divied class
+
+class advanceCalc          //  is going to inherit features adn properties from Calc
+{
+
+}
+
+
+
+
+ */
+
+/*              Inheritance
+ * 
+ * exemple
+ * is = our parents proporites is your own properties 
+ * has = 
+ * 
+ * 
+ */
---
+++ inheritance.java
@@ -0,0 +1,18 @@
+
+
+public class inheritance 
+{
+    public static void main(String a[])
+     {
+        AdvancedCalc obj = new AdvancedCalc();
+        int r1 = obj.add(4, 5);
+        int r2 = obj.sub(7, 3);
+        int r3 = obj.multi(5, 3);
+        int r4 = obj.div(15, 4);
+
+
+
+        System.out.println(r1 + " "  + r2 + " " + r3 + " " + r4 );
+
+    }
+}
