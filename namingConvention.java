7e8277a688d5c9fe50ce54ed738bf8d721db0655
MrDevaa <d.codandabany@gmail.com>
2023-04-12 20:13:32 +0200

    Naming Convention in java

---
+++ namingConvention.java
@@ -0,0 +1,19 @@
+public class namingConvention {
+    public static void main (String a[])
+    {
+
+    }
+}
+
+
+// Camel casing
+
+// class and interface - Calc, Runable
+// variable and method - marks, show();
+// constants - PIE, BRAND
+
+// showMyMarks() 
+//MyData
+
+
+// age, DATA, Human() 
