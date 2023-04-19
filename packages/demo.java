ef383346822e33fea58de5eca215fe927f5daa20
MrDevaa <d.codandabany@gmail.com>
2023-04-19 23:41:42 +0200

    packages in java

---
+++ packages/demo.java
@@ -0,0 +1,26 @@
+package com.google.calculation;
+
+import java.lang.*;
+
+// import in java
+import other.*;
+import other.tools.*;
+
+
+
+
+public class demo 
+{
+    public static void main(String a[])
+     {
+
+        Calc obj = new Calc();
+        AdvCalc obj1 = new AdvCalc();
+
+        A obj2 = new A();
+
+        System.out.println();
+    }
+
+
+}
